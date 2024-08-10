f24351c9689360566388813dad5e34ad6518ae20
plll0123 <jsh951227@gmail.com>
2024-08-11 01:07:36 +0900

    add s3 task

--- client/src/main/kotlin/com/example/ecc_test/api/ClientApi.kt
+++ client/src/main/kotlin/com/example/ecc_test/api/ClientApi.kt
@@ -1,11 +1,14 @@
 package com.example.ecc_test.api
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ClientApi {
+class ClientApi(
+    @Value("\${role}") private val role: String,
+) {
 
     @GetMapping("/client")
-    fun clientApi() = ApiResponse("client api v1")
+    fun clientApi() = ApiResponse("$role api v1")
 }
